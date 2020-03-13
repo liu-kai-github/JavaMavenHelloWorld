adce6a8aa0fa3d6c5babf757c336f4c900f7d732
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-03-13 18:41:58 +0800

    Initial commit

---
+++ src/main/java/com/myTest/App.java
@@ -0,0 +1,14 @@
+package com.myTest;
+
+import static strman.Strman.append;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        String s = append("f", "o", "o", "b", "a", "r");
+        System.out.println(s);
+        System.out.println("Hello World!");
+    }
+}
